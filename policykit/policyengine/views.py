7e754bbec403df19af4e49fc7abf2d0d5d9964d4
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-05 19:28:03 -0700

    add import statements

--- policykit/policyengine/views.py
+++ policykit/policyengine/views.py
@@ -34,7 +34,7 @@ def editor(request):
         'user': get_user(request)
     })
 
-def exec_code(policy, code, wrapperStart, wrapperEnd, globals=None, locals=None):
+def exec_code(policy, action, code, wrapperStart, wrapperEnd, globals=None, locals=None):
     from policyengine.models import Proposal, CommunityUser, BooleanVote, NumberVote
 
     errors = filter_code(code)
@@ -63,7 +63,7 @@ def filter_policy(policy, action):
 
     wrapper_end = "\r\nfilter_pass = filter(policy, action)"
 
-    exec_code(policy, policy.filter, wrapper_start, wrapper_end, None, _locals)
+    exec_code(policy, action, policy.filter, wrapper_start, wrapper_end, None, _locals)
 
     if _locals.get('filter_pass'):
         return _locals['filter_pass']
@@ -78,17 +78,13 @@ def initialize_policy(policy, action):
 
     wrapper_end = "\r\ninitialize(policy, action)"
 
-    exec_code(policy, policy.initialize, wrapper_start, wrapper_end, _globals, _locals)
+    exec_code(policy, action, policy.initialize, wrapper_start, wrapper_end, _globals, _locals)
 
     policy.has_notified = True
     policy.save()
 
 def check_policy(policy, action):
-    from policyengine.models import Proposal, CommunityUser, BooleanVote, NumberVote
 
-    users = CommunityUser.objects.filter(community=policy.community)
-    boolean_votes = BooleanVote.objects.filter(proposal=action.proposal)
-    number_votes = NumberVote.objects.filter(proposal=action.proposal)
 
     _locals = locals()
 
@@ -97,7 +93,7 @@ def check_policy(policy, action):
 
     wrapper_end = "\r\npolicy_pass = check(policy, action, users, boolean_votes, number_votes)"
 
-    exec_code(policy, policy.check, wrapper_start, wrapper_end, None, _locals)
+    exec_code(policy, action, policy.check, wrapper_start, wrapper_end, None, _locals)
 
     if _locals.get('policy_pass'):
         return _locals['policy_pass']
@@ -111,7 +107,7 @@ def notify_policy(policy, action):
 
     wrapper_end = "\r\nnotify(policy, action)"
 
-    exec_code(policy, policy.notify, wrapper_start, wrapper_end, None, _locals)
+    exec_code(policy, action, policy.notify, wrapper_start, wrapper_end, None, _locals)
 
 def pass_policy(policy, action):
     _locals = locals()
@@ -121,7 +117,7 @@ def pass_policy(policy, action):
     wrapper_end = "\r\nsuccess(policy, action)"
 
     logger.info('about to run exec code')
-    exec_code(policy, policy.success, wrapper_start, wrapper_end, None, _locals)
+    exec_code(policy, action, policy.success, wrapper_start, wrapper_end, None, _locals)
 
 def fail_policy(policy, action):
     _locals = locals()
@@ -130,7 +126,7 @@ def fail_policy(policy, action):
 
     wrapper_end = "\r\nfail(policy, action)"
 
-    exec_code(policy, policy.fail, wrapper_start, wrapper_end, None, _locals)
+    exec_code(policy, action, policy.fail, wrapper_start, wrapper_end, None, _locals)
 
 def clean_up_proposals(action, executed):
     from policyengine.models import Proposal, PlatformActionBundle
