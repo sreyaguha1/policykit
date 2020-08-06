e85361f68c4f65c3d75d165ea2e4babf6e02b92b
Sreya Guha <66488292+sreyaguha1@users.noreply.github.com>
2020-08-05 19:29:24 -0700

    add import statements

--- policykit/policyengine/views.py
+++ policykit/policyengine/views.py
@@ -3,7 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
-from policyengine.filter import *
+#from policyengine.filter import *
 from policykit.settings import SERVER_URL
 import urllib.request
 import urllib.parse
